4afd849efbd777efd8168dc539f50a8146f2c1f5
Kris Wang <wangzicong@nudt.edu.cn>
2016-09-04 14:42:50 +0800

    Update run-cmesh.sh to multiple lines. Fix executing "./run-cmesh.sh" path related problem in run-and-monitor.sh

--- run-and-monitor.sh
+++ run-and-monitor.sh
@@ -1,30 +1,35 @@
 #!/bin/bash
 {
     cd $HOME/gem5
-    run-cmesh.sh
-} &
-{
-    cd $HOME/gem5-origin
-    run-cmesh.sh
+    ./run-cmesh.sh
 } &> /dev/pts/1 &
 
-flag=1
-while [ $flag -eq 1 ]
-do
-    sleep 1s
-    pid=`pidof gem5.opt`
-    if [ -z "$pid" ]; then
-        echo "Running gem5 is completed."
-        tput bel
-        flag=0
-    fi
-done
+{
+    cd $HOME/gem5-origin
+    ./run-cmesh.sh
+} &> /dev/pts/2 &
 
-stats1=$HOME/gem5/m5out/stats.txt
-stats2=$HOME/gem5-origin/m5out/stats.txt
-if [ -s $stats1 ] && [ -s $stats2 ]; then
-    copy-to-datum.sh
-    compare-stats.sh
-else
-    echo "One(Both) stats.txt is empty, done."
-fi
+{
+    echo "Monitoring gem5 status..."
+    echo "I will notice you as long as workload gets completed."
+    flag=1
+    while [ $flag -eq 1 ]
+    do
+        sleep 1s
+        pid=`pidof gem5.opt`
+        if [ -z "$pid" ]; then
+            echo "Running gem5 is completed."
+            tput bel
+            flag=0
+        fi
+    done
+
+    stats1=$HOME/gem5/m5out/stats.txt
+    stats2=$HOME/gem5-origin/m5out/stats.txt
+    if [ -s $stats1 ] && [ -s $stats2 ]; then
+        copy-to-datum.sh
+        compare-stats.sh
+    else
+        echo "One(Both) stats.txt is empty, done."
+    fi
+} &
--- run-cmesh.sh
+++ run-cmesh.sh
@@ -1,4 +1,17 @@
 #!/bin/bash
 rm m5out/*
 cp run-cmesh.sh m5out
-./build/ALPHA_MESI_Two_Level/gem5.opt --outdir=m5out configs/example/run_spec2006.py --benchmarks=astar,astar,astar,astar,astar,astar,astar,astar,astar,astar,astar,astar,astar,astar,astar,astar,astar,astar,astar,astar,astar,astar,astar,astar,astar,astar,astar,astar,astar,astar,astar,astar --cpu-type=detailed --caches --cacheline_size=128 --l1i_size=16kB --l1i_assoc=2 --l1d_size=16kB --l1d_assoc=2 --l2cache --l2_size=128kB --l2_assoc=4 --num-l2caches=16 --num-cpus=32 --ruby --topology=CMesh --concentration-factor=2 --mesh-rows=4 --num-dirs=4 --mem-size=16GB --sys-clock=1GHz --ruby-clock=1GHz --cpu-clock=1GHz --output=m5out --errout=m5out --maxinsts=1000000 --garnet-network=fixed |tee m5out/runscript.log
+./build/ALPHA_MESI_Two_Level/gem5.opt \
+--outdir=m5out configs/example/run_spec2006.py \
+--benchmarks=specrand_i,specrand_i,specrand_i,specrand_i,specrand_i,specrand_i,specrand_i,specrand_i,specrand_i,specrand_i,specrand_i,specrand_i,specrand_i,specrand_i,specrand_i,specrand_i,specrand_i,specrand_i,specrand_i,specrand_i,specrand_i,specrand_i,specrand_i,specrand_i,specrand_i,specrand_i,specrand_i,specrand_i,specrand_i,specrand_i,specrand_i,specrand_i \
+--cpu-type=detailed --ruby --num-cpus=32 \
+--caches --cacheline_size=128 \
+--l1i_size=16kB --l1i_assoc=2 \
+--l1d_size=16kB --l1d_assoc=2 \
+--l2cache --l2_size=128kB --l2_assoc=4 --num-l2caches=16 \
+--topology=CMesh --concentration-factor=2 --mesh-rows=4 \
+--num-dirs=4 --mem-size=16GB \
+--sys-clock=1GHz --ruby-clock=1GHz --cpu-clock=1GHz \
+--output=m5out --errout=m5out \
+--maxinsts=1000000 \
+--garnet-network=fixed |tee m5out/runscript.log
